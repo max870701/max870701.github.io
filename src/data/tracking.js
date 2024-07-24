ceb495300ce469df3e26a92a3fe07729a14bd361
Hsieh <max870701@gmail.com>
2024-07-24 02:09:16 -0700

    fix: env

--- src/data/tracking.js
+++ src/data/tracking.js
@@ -1,3 +1,3 @@
 // Google Analytics Tracking ID process.env.GA_MEASUREMENT_ID
 // https://github.com/truethari/reactfolio#-google-analytics
-export const TRACKING_ID = process.env.GA4_MEASUREMENT_ID;
+export const TRACKING_ID = `${process.env.GA4_MEASUREMENT_ID}`;
