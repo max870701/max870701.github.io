9baf2fe17ec43309f32ecc0c5749b4e93377acc8
Hsieh <max870701@gmail.com>
2024-07-24 01:15:46 -0700

    fix: Fix the useLocation problem.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import ReactGA from "react-ga4";
 
 import Homepage from "./pages/homepage";
